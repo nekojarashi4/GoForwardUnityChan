9ebe090f5ff282dbb4646fa3127af0f415aef4fa
nekojarashi4 <mouryoutubasa@keroro.keroro.com>
2018-11-27 17:42:47 +0900

    2018.11.27 1回め

---
+++ Assets/CubeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = -0.2f;
+
+    private float deadLine = -10;
+
+    private float groundLevel = -3.0f;
+
+    public AudioSource AS;
+
+    public AudioClip A;
+
+    // Use this for initialization
+    void Start () {
+        AS = GetComponent<AudioSource>();
+        AS.clip = A;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(this.speed, 0, 0);
+
+        bool isGround = (transform.position.y >= this.groundLevel) ? false : true;
+
+        //if (transform.position.y <= this.groundLevel)
+        //{
+            GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+        //}
+
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground" || other.gameObject.tag == "Cube")
+        {
+            //AS.PlayOneShot(A);
+            AS.Play();
+        }
+    }
+}
